99d4a2103ec503e3f78a32762f3fc8f1e142fa63
3120005347马冀远 <1292048479@qq.com>
2024-03-19 09:05:51 +0800

    add a function of converting station location to Excel worksheet

--- divvy_json2excel.py
+++ divvy_json2excel.py
@@ -2,14 +2,17 @@ import pandas as pd
 import json
 
 # 从外部JSON文件读取数据
-with open('station_20240317.json', 'r', encoding='utf-8') as file:
+with open('station_20240316.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
 
 # 将数据转换为DataFrame
-df = pd.DataFrame(data)
+#df = pd.DataFrame(data)
 
 # 选择需要的字段
-df = df[["stationName", "bikesAvailable", "bikeDocksAvailable", "ebikesAvailable", "scootersAvailable", "totalRideablesAvailable", "isLightweight"]]
+#df = df[["stationName", "bikesAvailable", "bikeDocksAvailable", "ebikesAvailable", "scootersAvailable", "totalRideablesAvailable", "isLightweight"]]
+df = pd.DataFrame([(d['stationName'], d['location']['lat'], d['location']['lng']) for d in data],
+                  columns=['stationName', 'lat', 'lng'])
+
 
 # 将数据写入Excel文件
-df.to_excel("station_data_20240317.xlsx", index=False)
+df.to_excel("station_direction_20240316.xlsx", index=False)
