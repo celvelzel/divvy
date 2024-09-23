2330f30234d6ba34e679d439f399178405411016
celcelcel <rickymjy@gmail.com>
2024-09-23 14:07:12 +0800

    增加 添加站点坐标自定义文件

--- add_station_coord.py
+++ add_station_coord.py
@@ -3,18 +3,24 @@ import os
 from tqdm import tqdm
 
 # 指定文件夹路径
-folder_path = 'E:\\作品集\\三\\数据\\新建文件夹'
+folder_path = 'C:\\Users\\celcelcel\\Downloads\\divvy'
+# 站点信息文件名称
+stations_file = 'Divvy_Stations_2017_Q3Q4.csv'
+# 站点信息文件各字段名称
+station_fields = ['name', 'latitude', 'longitude']
+# 行程文件站点字段名称
+trip_fields = 'from_station_name'
 
 # 创建输出文件夹
 output_folder = os.path.join(folder_path, '添加站点坐标后的文件')
 os.makedirs(output_folder, exist_ok=True)
 
 # 获取文件夹中的所有CSV文件
-csv_files = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
+csv_files = [f for f in os.listdir(folder_path) if f.endswith('.csv') and f != stations_file]
 
 # 读取站点数据并将其存入字典中
-stations_df = pd.read_csv(os.path.join(folder_path, 'stations.csv'))  # 站点数据文件
-station_dict = dict(zip(stations_df['station_name'], zip(stations_df['longitude'], stations_df['latitude'])))
+stations_df = pd.read_csv(os.path.join(folder_path, stations_file))  # 站点数据文件
+station_dict = dict(zip(stations_df[station_fields[0]], zip(stations_df[station_fields[1]], stations_df[station_fields[2]])))
 
 
 # 函数：处理单个骑行数据文件
@@ -28,8 +34,8 @@ def process_file(file_path, station_dict):
         return
 
     # 如果不包含经纬度，添加经纬度列
-    rides_df['longitude'] = rides_df['station_name'].map(lambda x: station_dict.get(x, (None, None))[0])
-    rides_df['latitude'] = rides_df['station_name'].map(lambda x: station_dict.get(x, (None, None))[1])
+    rides_df['latitude'] = rides_df[trip_fields].map(lambda x: station_dict.get(x, (None, None))[0])
+    rides_df['longitude'] = rides_df[trip_fields].map(lambda x: station_dict.get(x, (None, None))[1])
 
     # 保存处理后的文件
     # 生成唯一的输出文件名
