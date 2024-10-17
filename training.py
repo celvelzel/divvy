d06b498d6e912dbda23c46d87670e567ed164ca4
celvelzel <rickymjy@foxmail.com>
2024-10-17 16:31:08 +0800

    新增 随机森林模型训练代码

--- training.py
+++ training.py
@@ -3,20 +3,22 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
-path = 'C:\\Users\\Administrator\\Desktop\\RFtest.csv' #数据路径
-rawdata = pd.read_csv(path)  #加载数据
-x = rawdata.drop('y_value',axis = 1) #输入特征
-y = rawdata['y_value']  #目标变量
-x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 0.3,random_state=0) #30%为测试集，则70%为训练集
-rfc = RandomForestClassifier()  #使用默认随机森林分类器
-rfc.fit(x_train,y_train) #使用训练数据集训练随机森林模型
-y_pred = rfc.predict(x_test) #使用分类器预测测试集的类别
-importances = rfc.feature_importances_ # 计算特征重要性
+
+path = 'data/dataset/grid_poi_counts.csv'  # 数据路径
+rawdata = pd.read_csv(path)  # 加载数据
+rawdata.drop(columns=['grid_id', 'geometry'], inplace=True)
+x = rawdata.drop('trip_count', axis=1)  # 输入特征
+y = rawdata['trip_count']  # 目标变量
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)  # 30%为测试集，则70%为训练集
+rfc = RandomForestClassifier()  # 使用默认随机森林分类器
+rfc.fit(x_train, y_train)  # 使用训练数据集训练随机森林模型
+y_pred = rfc.predict(x_test)  # 使用分类器预测测试集的类别
+importances = rfc.feature_importances_  # 计算特征重要性
 print("Importances")
 print(importances)
 print("Confusion Matrix:")
 print(confusion_matrix(y_test, y_pred))  # 输出混淆矩阵
 print("Classification Report:")
 print(classification_report(y_test, y_pred))  # 输出分类报告
-print("Accuracy:") #输出精度
+print("Accuracy:")  # 输出精度
 print(accuracy_score(y_test, y_pred))
