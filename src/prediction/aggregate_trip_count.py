ec835eeffdc620d19fc9d12fc237c5c5edb74d6e
celvelzel <74584720+celvelzel@users.noreply.github.com>
2024-11-08 16:52:10 +0800

    Update aggregate_trip_count.py

--- src/prediction/aggregate_trip_count.py
+++ src/prediction/aggregate_trip_count.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 date_pattern = r'\d{4}-\d{2}-\d{2}'
 
 # 定义输入文件夹路径和输出文件路径
-input_folder = '../../data/trip_count'
+input_folder = '../../output/reasoning_result'
 output_file = '../../output/aggregated_trip_counts.csv'
 
 # 初始化空列表，用于存储每个周的数据
@@ -16,15 +16,17 @@ weekly_data = []
 for file_name in tqdm(sorted(os.listdir(input_folder))):
     if file_name.endswith('.csv'):
         # 提取周标识，例如 'week1' 等，作为行索引
-        date = re.search(date_pattern, file_name)
-        week = date
+        match = re.search(date_pattern, file_name)
+        if match:
+            date = match.group()
+            week = date
 
         # 读取 CSV 文件
         file_path = os.path.join(input_folder, file_name)
-        df = pd.read_csv(file_path, usecols=['grid_id', 'trip_counts'])
+        df = pd.read_csv(file_path, usecols=['grid_id', 'trip_count'])
 
         # 将区块数据转换为一行，以 `grid_id` 作为列名，`trip_counts` 作为值
-        week_series = df.set_index('grid_id')['trip_counts']
+        week_series = df.set_index('grid_id')['trip_count']
         week_series.name = week  # 行名设置为周标识
 
         # 将周数据追加到列表中
